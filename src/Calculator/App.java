ff13975b0d79924a496b2400d3209784f0cae9b2
jkhan94 <hjkwork@naver.com>
2024-04-23 21:32:39 +0900

    1-7. 연산 결과가 10개로 고정되지 않고 무한이 저장될 수 있도록 소스 코드를 수정             - JCF(Java Collection Framework)를 사용합니다. (e.g. List, Set )             - “remove”라는 문자열을 입력받으면 가장 먼저 저장된 결과가 삭제될 수 있도록 구현.

--- src/Calculator/App.java
+++ src/Calculator/App.java
@@ -1,18 +1,17 @@
 package Calculator;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class App {
     public static void main(String[] args) {
         int num1, num2;
         char operator;
-        String repeat;
-        int index = 0;
-        double[] operResult = new double[10];
+        String repeat, removeVal;
+        LinkedList<Double> operResult = new LinkedList<Double>();
 
         // 1-4. 반복문(e.g. for, while)을 사용하여 반복의 종료를 알려주는 “exit” 문자열을 입력하기 전까지 무한으로 계산을 진행할 수 있도록 수정
         while (true) {
-            int result = 0;
+            double result = 0.0;
 
             // 1-1. Scanner를 사용하여 양의 정수 2개(0 포함)를 전달 받음.
             Scanner sc = new Scanner(System.in);
@@ -56,16 +55,16 @@ public class App {
             - 연산의 결과를 비어있는 곳에 저장하기 위해 저장할 때마다 count 합니다.*/
             /*1-6. 연산 결과가 10개 초과하면 가장 먼저 저장된 결과를 삭제하고 새로운 연산 결과가 저장될 수 있도록 소스 코드를 수정.
             - 현재 저장된 index가 마지막(9)라면 가장 먼저 저장된 결과 값이 삭제 되고 새로운 결과 값이 마지막 index에 저장될 수 있도록 구현.*/
-            if (index < 10) {
-                // 배열이 비어 있으면 결과 저장.
-                operResult[index] = result;
-                index++;
-            } else if (index >= 10) {
-                // 배열이 꽉 찼으면 앞으로 당김.
-                for (int i = 0; i < 9; i++) {
-                    operResult[i] = operResult[i + 1];
-                }
-                operResult[9] = result;
+            /*1-7. 연산 결과가 10개로 고정되지 않고 무한이 저장될 수 있도록 소스 코드를 수정
+            - JCF(Java Collection Framework)를 사용합니다. (e.g. List, Set )
+            - “remove”라는 문자열을 입력받으면 가장 먼저 저장된 결과가 삭제될 수 있도록 구현. */
+            // 리스트에 연산 결과 저장
+            operResult.add(result);
+
+            System.out.println("가장 먼저 저장된 연산 결과를 삭제하려면 remove를 입력하세요. (삭제하지 않으려면 아무 키나 누르세요)");
+            removeVal = sc.next();
+            if (removeVal.equals("remove")) {
+                operResult.remove(0);
             }
 
             System.out.println("더 계산하시려면 아무 키나 누르세요. (exit 입력 시 종료)");
