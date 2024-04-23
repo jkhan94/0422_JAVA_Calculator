41645a4f621354b6a64694ddcd419e8aa4f49cc9
jkhan94 <hjkwork@naver.com>
2024-04-23 20:28:25 +0900

    1-2. Scanner를 사용하여 사칙연산 기호를 전달 받음. (`charAt(0)`)

--- src/Calculator/App.java
+++ src/Calculator/App.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        // 1. Scanner를 사용하여 양의 정수 2개(0 포함)를 전달 받음.
+        // 1-1. Scanner를 사용하여 양의 정수 2개(0 포함)를 전달 받음.
         Scanner sc = new Scanner(System.in);
         int num1, num2;
         System.out.print("첫 번째 숫자를 입력하세요: ");
@@ -12,6 +12,7 @@ public class App {
         System.out.print("두 번째 숫자를 입력하세요: ");
         num2 = sc.nextInt();
 
-
+        // 1-2. Scanner를 사용하여 사칙연산 기호를 전달 받음. (`charAt(0)`)
+        char operator = sc.next().charAt(0);
     }
 }
