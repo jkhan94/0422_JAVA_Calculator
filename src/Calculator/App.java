e30ccb2744df686f1139e4b5006aef4c4d927de1
jkhan94 <hjkwork@naver.com>
2024-04-23 20:25:31 +0900

    1-1. Scanner를 사용하여 양의 정수 2개(0 포함)를 전달 받음.

---
+++ src/Calculator/App.java
@@ -0,0 +1,17 @@
+package Calculator;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        // 1. Scanner를 사용하여 양의 정수 2개(0 포함)를 전달 받음.
+        Scanner sc = new Scanner(System.in);
+        int num1, num2;
+        System.out.print("첫 번째 숫자를 입력하세요: ");
+        num1 = sc.nextInt();
+        System.out.print("두 번째 숫자를 입력하세요: ");
+        num2 = sc.nextInt();
+
+
+    }
+}
