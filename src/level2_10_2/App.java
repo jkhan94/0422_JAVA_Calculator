9e28d3243181049e96c27637c29f1211ff47652b
jkhan94 <hjkwork@naver.com>
2024-04-25 14:27:48 +0900

    App.java main 함수에서 if문을 switch~case문으로 변경.

--- src/level2_10_2/App.java
+++ src/level2_10_2/App.java
@@ -50,84 +50,89 @@ public class App {
             operTypes.printOperTypes();
             operType = sc.next();
 
+            switch (operType) {
 // 사칙연산
-            if (operType.equals("사칙연산")) {
-                // 양의 정수를 입력받을 때까지 반복
-                do {
-                    System.out.print("첫 번째 양의 정수를 입력하세요: ");
-                    num1 = sc.nextInt();
-                    System.out.print("두 번째 양의 정수를 입력하세요: ");
-                    num2 = sc.nextInt();
-                } while (num1 < 0 || num2 < 0);
+                case "사칙연산":
+                    // 양의 정수를 입력받을 때까지 반복
+                    do {
+                        System.out.print("첫 번째 양의 정수를 입력하세요: ");
+                        num1 = sc.nextInt();
+                        System.out.print("두 번째 양의 정수를 입력하세요: ");
+                        num2 = sc.nextInt();
+                    } while (num1 < 0 || num2 < 0);
 
-                // Scanner를 사용하여 사칙연산 기호를 전달 받음. (`charAt(0)`)
-                System.out.print("사칙연산 기호를 입력하세요 (+ - * / %) : ");
-                operator = sc.next().charAt(0);
+                    // Scanner를 사용하여 사칙연산 기호를 전달 받음. (`charAt(0)`)
+                    System.out.print("사칙연산 기호를 입력하세요 (+ - * / %) : ");
+                    operator = sc.next().charAt(0);
 
-                // 스트링.indexOf():  연산기호 문자열 oper에 입력된 연산기호가 없으면 -1을 리턴
-                try {
-                    if (oper.indexOf(operator) < 0) {
-                        throw new BadOperatorException();
-                    }
-                } catch (BadOperatorException e) {
-                    System.out.println(e.getMessage());
-                }
-
-                // 입력된 연산기호를 바탕으로 연산 수행
-                switch (operator) {
-                    case '+':
-                        arithResult.add(ariCalc.add(num1, num2));
-                        break;
-                    case '-':
-                        arithResult.add(ariCalc.sub(num1, num2));
-                        break;
-                    case '*':
-                        arithResult.add(ariCalc.mul(num1, num2));
-                        break;
-                    case '/':
-                        try {
-                            if (num2 == 0) {
-                                throw new DivisionException();
-                            }
-                            arithResult.add(ariCalc.div(num1, num2));
-                        } catch (DivisionException e) {
-                            System.out.println(e.getMessage());
+                    // 스트링.indexOf():  연산기호 문자열 oper에 입력된 연산기호가 없으면 -1을 리턴
+                    try {
+                        if (oper.indexOf(operator) < 0) {
+                            throw new BadOperatorException();
                         }
-                        break;
-                    case '%':
-                        arithResult.add(ariCalc.mod(num1, num2));
-                        break;
-                }
-                ariCalc.setOperResult(arithResult);
+                    } catch (BadOperatorException e) {
+                        System.out.println(e.getMessage());
+                    }
+
+                    // 입력된 연산기호를 바탕으로 연산 수행
+                    switch (operator) {
+                        case '+':
+                            arithResult.add(ariCalc.add(num1, num2));
+                            break;
+                        case '-':
+                            arithResult.add(ariCalc.sub(num1, num2));
+                            break;
+                        case '*':
+                            arithResult.add(ariCalc.mul(num1, num2));
+                            break;
+                        case '/':
+                            try {
+                                if (num2 == 0) {
+                                    throw new DivisionException();
+                                }
+                                arithResult.add(ariCalc.div(num1, num2));
+                            } catch (DivisionException e) {
+                                System.out.println(e.getMessage());
+                            }
+                            break;
+                        case '%':
+                            arithResult.add(ariCalc.mod(num1, num2));
+                            break;
+                    }
+                    ariCalc.setOperResult(arithResult);
 //                System.out.println(ariCalc.getOperResult());
 
-                // 가장 오래된 연산 결과 삭제
-                System.out.println("가장 먼저 저장된 연산 결과를 삭제하려면 remove를 입력하세요. (삭제하지 않으려면 아무 키나 누르세요)");
-                removeVal = sc.next();
-                if (removeVal.equals("remove")) {
-                    ariCalc.removeResult();
-                }
+                    // 가장 오래된 연산 결과 삭제
+                    System.out.println("가장 먼저 저장된 연산 결과를 삭제하려면 remove를 입력하세요. (삭제하지 않으려면 아무 키나 누르세요)");
+                    removeVal = sc.next();
+                    if (removeVal.equals("remove")) {
+                        ariCalc.removeResult();
+                    }
 
-                // 저장된 연산 결과 출력
-                System.out.println("저장된 연산결과를 조회하시려면 inquiry를 입력하세요. (조회하지 않으려면 아무 키나 누르세요)");
-                printResult = sc.next();
-                if (printResult.equals("inquiry")) {
-                    ariCalc.inquiryResults();
-                }
-            } else if (operType.equals("원의넓이")) {
-                // 양의 정수 입력될 때까지 반복 (0 포함)
-                do {
-                    System.out.print("원의 반지름을 입력하세요: ");
-                    num1 = sc.nextInt();
-                } while (num1 < 0);
-                // 원의 넓이 계산
-                circleResult.add(cirCalc.getArea(num1));
-                // 원의 넓이 저장
-                cirCalc.setOperResult(circleResult);
+                    // 저장된 연산 결과 출력
+                    System.out.println("저장된 연산결과를 조회하시려면 inquiry를 입력하세요. (조회하지 않으려면 아무 키나 누르세요)");
+                    printResult = sc.next();
+                    if (printResult.equals("inquiry")) {
+                        ariCalc.inquiryResults();
+                    }
+                    break;
+// 원의 넓이
+                case "원의넓이":
+                    // 양의 정수 입력될 때까지 반복 (0 포함)
+                    do {
+                        System.out.print("원의 반지름을 입력하세요: ");
+                        num1 = sc.nextInt();
+                    } while (num1 < 0);
+                    // 원의 넓이 계산
+                    circleResult.add(cirCalc.getArea(num1));
+                    // 원의 넓이 저장
+                    cirCalc.setOperResult(circleResult);
 //              System.out.println(cirCalc.getOperResult());
-                // 저장된 연산 결과 출력
-                cirCalc.inquiryResults();
+                    // 저장된 연산 결과 출력
+                    cirCalc.inquiryResults();
+                    break;
             }
+
 
             // 가능한 연산 종류가 입력됐을 경우 계속 계산할 건지 결정.
             if (operType.equals("사칙연산") || operType.equals("원의넓이")) {
